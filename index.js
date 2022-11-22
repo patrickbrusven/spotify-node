146dad7d34ffa3a49a75cd7d5a9a09b47d17bd16
patrickbrusven <pjbrusven@gmail.com>
2022-11-22 12:12:44 -0800

    generateRandomString function for stateKey cookie

--- index.js
+++ index.js
@@ -8,11 +8,25 @@ const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+ const generateRandomString = length => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = 'spotify_auth_state';
 
 app.get('/login', (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
   const queryParams = querystring.stringify({
     client_id: CLIENT_ID,
     redirect_uri: REDIRECT_URI,
