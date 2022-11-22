38b59deec83a4c59caf360c0d32a705707fdd1a2
patrickbrusven <pjbrusven@gmail.com>
2022-11-22 11:58:57 -0800

    login and callback route

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ require('dotenv').config()
 const app = express()
 const port = 8080
 
-const clientID = process.env.REACT_APP_CLIENT_ID
-const clientSecret = process.env.REACT_APP_CLIENT_SECRET
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 app.get('/', (req, res) => {
-  res.send(clientID + 'hello world')
+  res.send('hello world')
+})
+
+app.get('/login', (req, res) => {
+  res.redirect('https://accounts.spotify.com/authorize?' + `client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=user-read-private%20user-read-email`);
+})
+
+app.get('/callback', function(req, res) {
+  var code = req.query.code || null;
+  res.send(code);
 })
 
 app.listen(port, () => {
